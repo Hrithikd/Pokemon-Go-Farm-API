a3b29309764e4d540bb3d7884e0b6ad325822543
Hrithik Datta <hrithikd@berkeley.edu>
2020-07-10 01:17:50 -0700

    squashed commits

---
+++ PokeBot.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+"""
+pgoapi - Pokemon Go API
+Copyright (c) 2016 tjado <https://github.com/tejado>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+OR OTHER DEALINGS IN THE SOFTWARE.
+
+Author: tjado <https://github.com/tejado>
+
+P.S. Not all of this is Tejados
+"""
+
+import os
+import re
+import json
+import struct
+import logging
+import requests
+import argparse
+from time import sleep
+from API import PGoApi
+from API.utilities import f2i, h2f
+from API.location import getNeighbors
+
+from google.protobuf.internal import encoder
+from geopy.geocoders import GoogleV3
+from s2sphere import CellId, LatLng
+
+log = logging.getLogger(__name__)
+
+def get_pos_by_name(location_name):
+    geolocator = GoogleV3()
+    loc = geolocator.geocode(location_name)
+
+#log.info('Your given location: %s', loc.address.encode('utf-8'))
+#log.info('lat/long/alt: %s %s %s', loc.latitude, loc.longitude, loc.altitude)
+    return (12, 123, 0) # CHange this to ure long or lat or uncomment below stm if u have google geologcater api
+    #return (loc.latitude, loc.longitude, loc.altitude)
+
+def get_cellid(lat, long):
+    origin = CellId.from_lat_lng(LatLng.from_degrees(lat, long)).parent(15)
+    walk = [origin.id()]
+
+    # 10 before and 10 after
+    next = origin.next()
+    prev = origin.prev()
+    for i in range(10):
+        walk.append(prev.id())
+        walk.append(next.id())
+        next = next.next()
+        prev = prev.prev()
+    return ''.join(map(encode, sorted(walk)))
+
+def encode(cellid):
+    output = []
+    encoder._VarintEncoder()(output.append, cellid)
+    return ''.join(output)
+
+def init_config():
+    parser = argparse.ArgumentParser()
+    config_file = "config.json"
+
+    # If config file exists, load variables from json
+    load   = {}
+    if os.path.isfile(config_file):
+        with open(config_file) as data:
+            load.update(json.load(data))
+
+    # Read passed in Arguments
+    required = lambda x: not x in load['accounts'][0].keys()
+    parser.add_argument("-a", "--auth_service", help="Auth Service ('ptc' or 'google')",
+        required=required("auth_service"))
+    parser.add_argument("-i", "--config_index", help="config_index", required=required("config_index"))
+    parser.add_argument("-u", "--username", help="Username", required=required("username"))
+    parser.add_argument("-p", "--password", help="Password", required=required("password"))
+    parser.add_argument("-l", "--location", help="Location", required=required("location"))
+    parser.add_argument("-d", "--debug", help="Debug Mode", action='store_true')
+    parser.add_argument("-c", "--cached", help="cached", action='store_true')
+    parser.add_argument("-t", "--test", help="Only parse the specified location", action='store_true')
+    parser.set_defaults(DEBUG=False, TEST=False,CACHED=False)
+    config = parser.parse_args()
+    load = load['accounts'][int(config.__dict__['config_index'])]
+    # Passed in arguments shoud trump
+    for key in config.__dict__:
+        if key in load and config.__dict__[key] == None:
+            config.__dict__[key] = load[key]
+
+    if config.auth_service not in ['ptc', 'google']:
+      log.error("Invalid Auth service specified! ('ptc' or 'google')")
+      return None
+
+    return config
+
+
+def main():
+    # log settings
+    # log format
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(module)10s] [%(levelname)5s] %(message)s')
+    # log level for http request class
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    # log level for main pgoapi class
+    logging.getLogger("pgoapi").setLevel(logging.INFO)
+    # log level for internal pgoapi class
+    logging.getLogger("rpc_api").setLevel(logging.INFO)
+
+    config = init_config()
+    if not config:
+        return
+
+    if config.debug:
+        logging.getLogger("requests").setLevel(logging.DEBUG)
+        logging.getLogger("pgoapi").setLevel(logging.DEBUG)
+        logging.getLogger("rpc_api").setLevel(logging.DEBUG)
+
+    position = get_pos_by_name(config.location)
+    if config.test:
+        return
+
+    # instantiate pgoapi
+    api = PGoApi()
+
+    # provide player position on the earth
+    api.set_position(*position)
+
+    if not api.login(config.auth_service, config.username, config.password, config.cached):
+        return
+
+    # chain subrequests (methods) into one RPC call
+
+    # get player profile call
+    # ----------------------
+    api.get_player()
+
+    # get inventory call
+    # ----------------------
+    api.get_inventory()
+
+    # get map objects call
+    # ----------------------
+    #timestamp = "\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000\000"
+    #cellid = get_cellid(position[0], position[1])
+    #api.get_map_objects(latitude=f2i(position[0]), longitude=f2i(position[1]), since_timestamp_ms=timestamp, cell_id=cellid)
+
+    # spin a fort
+    # ----------------------
+    #fortid = '<your fortid>'
+    #lng = <your longitude>
+    #lat = <your latitude>
+    #api.fort_search(fort_id=fortid, fort_latitude=lat, fort_longitude=lng, player_latitude=f2i(position[0]), player_longitude=f2i(position[1]))
+
+    # release/transfer a pokemon and get candy for it
+    # ----------------------
+    #api.release_pokemon(pokemon_id = <your pokemonid>)
+
+    # get download settings call
+    # ----------------------
+    #api.download_settings(hash="4a2e9bc330dae60e7b74fc85b98868ab4700802e")
+
+    # execute the RPC call
+    response_dict = api.call()
+    print('Response dictionary: \n\r{}'.format(json.dumps(response_dict, indent=2)))
+    # r = api.get_player().call()
+    # print(r)
+    # alternative:
+    # api.get_player().get_inventory().get_map_objects().download_settings(hash="4a2e9bc330dae60e7b74fc85b98868ab4700802e").call()
+    while True:
+        api.main_loop()
+        # print("Any pokemon caught? : ", api.catch_near_pokemon())
+        # print(api.spin_near_fort())
+        # try:
+        #     print(api.spin_near_fort())
+        # except Exception as e:
+        #     print("Something went wrong: ", e)
+        #     sleep(30)
+    import ipdb; ipdb.set_trace()
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Pokemon-Go-Bot-Working-Hack-API
+## Pokemon-Go-Working-Bot-Hack
+# Instructions
+1. Download/fork project; and open up the zip file inside the project and put it in a separate folder, separate from this original project.
+2. Open up zip and open up command line and go to the directory where the opened zip is at. 
+3. Run the following line on terminal/CMD: `pip install -r requirements.txt` If that doesn't work then just add `sudo` to the beginning and enter the password for your main computer account.
+4. Then go to the configure.json file and change whatever information you need. Then go to pokebot.py and change the latitude & longitude where stated.
+5. Run the following line on terminal/CMD if you login into Pokemon Go with Google: `python pokebot.py -i 0` and `python pokebot.py -i 1` if you use Pokemon Trainer's Club
+6. After you run it the first time add `--cache` to the end. For example, `python pokebot.py -i 0 --cache`
+7. Many thanks to the many devs at /r/pokemongodev!
